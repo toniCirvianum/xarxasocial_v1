442d09582c73a0e15407e4765595ae0918d9873a
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-11-25 20:27:49 +0100

    first commit

---
+++ database/migrations/2024_11_25_191208_add_columns_to_table_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username');
+            $table->string('nick');
+            $table->string('image');
+            // $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname','nick','image','remember_token');
+        });
+    }
+};
