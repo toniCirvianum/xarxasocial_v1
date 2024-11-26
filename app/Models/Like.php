0578f79ddbf661d3a3a7da00496a22a9a68d9528
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-11-26 19:04:58 +0100

    seed and factories ready

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
+    use HasFactory;
     protected $table = 'likes';
 
     public function user() {
---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' =>User::factory(),
+            'image_path' => fake()->imageUrl(640,480,'abstract',true,'laravel'),
+            'description' => fake()->sentence()
+        ];
+    }
+}
