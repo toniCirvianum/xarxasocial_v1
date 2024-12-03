bb788c3254eaee506771f4cc732293f584a7b0df
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-12-03 16:21:47 +0100

    like controller ready

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,49 +8,59 @@ use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    // public function like($id) {
-
-    //     $isset_like= Like ::where('user_id',Auth::user()->id)->where('image_id',$id)->count();
-    //     $user = Auth::user();
-           
-    //     if ($isset_like == 0) {
-    //         $like = Like::create([
-    //             'user_id' =>$user->id,
-    //             'image_id' => $id
-    //         ]);
-    //         echo "Like Afegit";
-    //     } else {
-    //         $like= Like ::where('user_id',Auth::user()->id)->where('image_id',$id)->first();
-    //         $like -> delete();
-    //         echo "like eliminat";
-            
-    //     }
-
-    // }
-
     public function like($id)
     {
 
         $isset_like = Like::where('user_id', Auth::user()->id)->where('image_id', $id)->count();
+        $user = Auth::user();
+
         if ($isset_like == 0) {
-            $user = Auth::user();
-            $like=Like::create([
+            $like = Like::create([
                 'user_id' => $user->id,
                 'image_id' => $id
             ]);
             return response()->json([
-                'like'=>$like,
-                'message'=>'Like fet'
+                'like' => $like,
+                'message' => 'Like fet'
             ]);
         } else {
+            $like = Like::where('user_id', Auth::user()->id)->where('image_id', $id)->first();
+            if ($like) {
+                $like->delete();
+                return response()->json([
+                    'like' => Like::find($id),
+                    'message' => 'Like eliminat'
+                ]);
+            } else {
 
-            return response()->json([
-                'message'=>'El like ja existeix'
-            ]);
+                return response()->json([
+                    'message' => 'El like no existeix'
+                ]);
+            }
         }
-
-        
     }
+
+    // public function like($id)
+    // {
+
+    //     $isset_like = Like::where('user_id', Auth::user()->id)->where('image_id', $id)->count();
+    //     if ($isset_like == 0) {
+    //         $user = Auth::user();
+    //         $like = Like::create([
+    //             'user_id' => $user->id,
+    //             'image_id' => $id
+    //         ]);
+    //         return response()->json([
+    //             'like' => $like,
+    //             'message' => 'Like fet'
+    //         ]);
+    //     } else {
+
+    //         return response()->json([
+    //             'message' => 'El like ja existeix'
+    //         ]);
+    //     }
+    // }
 
     public function dislike($id)
     {
@@ -59,15 +69,14 @@ class LikeController extends Controller
         if ($like) {
             $like->delete();
             return response()->json([
-                'like'=>Like::find($id),
-                'message'=>'Like eliminat'
+                'like' => Like::find($id),
+                'message' => 'Like eliminat'
             ]);
         } else {
 
             return response()->json([
-                'message'=>'El like no existeix'
+                'message' => 'El like no existeix'
             ]);
         }
-
     }
 }
